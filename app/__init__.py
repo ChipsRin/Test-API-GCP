5d6fe28204cf6ff65581dce45c8c617ca1ed9c95
ChipsRin <80757624+ChipsRin@users.noreply.github.com>
2023-03-15 12:45:20 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -45,6 +45,6 @@ def postInput():
 
     input = np.array([[x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24]])
     # 進行預測
-    result = model.predict(input)
+    result = model.predict(input,validate_features=False)
 
     return jsonify({'result': str(result)})
