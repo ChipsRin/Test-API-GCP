a53324fa6958198cf1595c08afd67238fd28d38d
ChipsRin <80757624+ChipsRin@users.noreply.github.com>
2023-03-23 10:09:25 +0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ CORS(app)
 
 @app.route('/test', methods=['GET'])
 def getResult():
-    input = np.array([[1, 66, 80, 126, 51, 74, 118, 40, 57, 15, 6.3, 118, 98.3, 0, 167, 91, 173, 30.4, 131, 85.6, 172, 2.45, 2.8, 0]])
+    input = np.array([[1, 66, 80, 118, 40, 57, 15, 6.3, 118, 98.3, 0, 91, 173, 30.4, 131, 85.6, 172, 2.45, 2.8, 0]])
     result = model.predict(input)
     return jsonify({'result': str(result)})
 
@@ -21,30 +21,26 @@ def postInput():
     x1=insertValues['gender']
     x2=insertValues['anchor_age']
     x3=insertValues['HR']
-    x4=insertValues['ABPs']
-    x5=insertValues['ABPd']
-    x6=insertValues['ABPm']
-    x7=insertValues['NBPs']
-    x8=insertValues['NBPd']
-    x9=insertValues['NBPm']
-    x10=insertValues['RR']
-    x11=insertValues['Hemoglobin']
-    x12=insertValues['Glucose (serum)']
-    x13=insertValues['Temperature F']
-    x14=insertValues['Unintentional weight loss >10 lbs.']
-    x15=insertValues['Glucose finger stick (range 70-100)']
-    x16=insertValues['Admission Weight (Kg)']
-    x17=insertValues['Height (cm)']
-    x18=insertValues['BMI']
-    x19=insertValues['AST']
-    x20=insertValues['ALT']
-    x21=insertValues['Alkaline Phosphate(ALP)']
-    x22=insertValues['Total Bilirubin']
-    x23=insertValues['Albumin']
-    x24=insertValues['Family_history']
+    x4=insertValues['NBPs']
+    x5=insertValues['NBPd']
+    x6=insertValues['NBPm']
+    x7=insertValues['RR']
+    x8=insertValues['Hemoglobin']
+    x9=insertValues['Glucose (serum)']
+    x10=insertValues['Temperature F']
+    x11=insertValues['Unintentional weight loss >10 lbs.']
+    x12=insertValues['Admission Weight (Kg)']
+    x13=insertValues['Height (cm)']
+    x14=insertValues['BMI']
+    x15=insertValues['AST']
+    x16=insertValues['ALT']
+    x17=insertValues['Alkaline Phosphate(ALP)']
+    x18=insertValues['Total Bilirubin']
+    x19=insertValues['Albumin']
+    x20=insertValues['Family_history']
 
-    input = np.array([[x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24]])
+    input = np.array([[x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20,]])
     # 進行預測
-    result = model.predict(input,validate_features=False)
+    result = model.predict(input)
 
     return jsonify({'result': str(result)})
